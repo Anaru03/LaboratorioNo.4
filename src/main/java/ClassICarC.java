7d1def8f2a736ccc6de8e15721160952ab251305
Ruth Anaí de León <dele22428@uvg.edu.gt>
2022-11-13 17:22:37 -0600

    Se desarrolló la función de encendido y apagado

--- src/main/java/ClassICarC.java
+++ src/main/java/ClassICarC.java
@@ -18,124 +18,246 @@ public class ClassICarC implements ICarC{
     private int volume;
     private float station;
     private int audioRepType;
-    ArrayList <ISong> AListOfSongs = new ArrayList<>();
-    private boolean phoneConnectionStatus;
-     
+    private int callStatus;
+    ArrayList <ISong> AListOfSongs;
+    ArrayList <String> ListofContacts;
     
     
+    public boolean modo = false;
+    
+  
+    /**
+     * 
+     * @return 
+     */
     @Override
-    public boolean SwitchRadioONOFF() {
+    public boolean SwitchRadioONOFF(boolean actual_state) {
+        if(actual_state) {
+            this.modo = false;
+        }else{
+            this.modo = true;
+        }
+      
+        return true;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public boolean getRadioStatus() {return radioStatus;
+        
+    }
+    
+    /**
+     * 
+     * @param RadioStatus 
+     */
+    @Override
+    public void setRadioStatus(boolean RadioStatus) {this.radioStatus = radioStatus;
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
-    public boolean getRadioStatus() {
+    public int getVolume() {return volume;
+        
+    }
+
+    /**
+     * 
+     * @param Volume 
+     */
+    @Override
+    public void setVolume(int Volume) {this.volume = volume;
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
-    public int getVolume() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void setVolume(int Volume) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
+    
+    /**
+     * 
+     * @param frequency
+     * @return 
+     */
     @Override
     public float SwitchFrequency(float frequency) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @param stationChange 
+     */
     @Override
-    public void setStation(float stationChange) {
+    public void setStation(float stationChange) {this.station = station;
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
-    public float getStation() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public float getStation() {return  station;
+        
     }
 
+    /**
+     * 
+     * @param _station 
+     */
     @Override
     public void SaveStation(float _station) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @param _StationIndex
+     * @return 
+     */
     @Override
     public float SelectSpecificStation(int _StationIndex) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
-    public int getAudioRepType() {
+    public int getAudioRepType() {return audioRepType;
+        
+    }
+
+    /**
+     * 
+     * @param _audioRepType 
+     */
+    @Override
+    public void setAudioRepType(int _audioRepType) {this.audioRepType = audioRepType;
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
-    public void setAudioRepType() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
+    /**
+     * 
+     * @param TypeOfAudioReproduction
+     * @return 
+     */
     @Override
     public ArrayList<ISong> getAListOfSongs(int TypeOfAudioReproduction) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return AListOfSongs;
+        
     }
 
+    /**
+     * 
+     * @param CurrentIndex
+     * @param TypeOfAudioReproduction 
+     */
     @Override
     public void NextSong(int CurrentIndex, int TypeOfAudioReproduction) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @param CurrentIndex
+     * @param TypeOfAudioReproduction 
+     */
     @Override
     public void PrevSong(int CurrentIndex, int TypeOfAudioReproduction) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @param TypeOfAudioReproduction
+     * @return 
+     */
     @Override
     public String PlaySong(int TypeOfAudioReproduction) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
     public boolean Connect_DisconnectPhone() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
     public boolean getPhoneConnectionStatus() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
     public ArrayList<String> getListofContacts() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return ListofContacts;
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
-    public int getCallStatus() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int getCallStatus() {return callStatus;
+        
     }
 
+    /**
+     * 
+     * @param NameforCall
+     * @return 
+     */
     @Override
     public String makeCall(String NameforCall) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
     public String PlaceCallOnHold() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
-    public String endCall() {
+    public String endCall() { 
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * 
+     * @return 
+     */
     @Override
     public String getWeatherForCast() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    
     
 }
